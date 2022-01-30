fd27e5803b87c0d6ca82ee7f43a25617703e9a9c
Christopher Weiss <chris.john.weiss@gmail.com>
2022-01-29 23:43:34 -0500

    commit tree fd08f44370fbe584dd19149df4aa78beb97ddd61

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  function sayHiToHeadphonedRoommate(string) {
+    return string."LetsHAveDinnerTogether!";
+  }
+const string = "HELLO!";
+
+console.log(string);
+console.log(string.toUpperCase());
+
+string === string.toUpperCase();
